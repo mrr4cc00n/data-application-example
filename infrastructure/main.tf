319ecbd12c7c40cbd2628e0705dad330e2e51b28
r4cc00n <manuel@tetra.team>
2022-01-21 09:30:57 +0100

    data application example
    project structure

---
+++ infrastructure/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.72.0"
+    }
+  }
+  backend "remote" {
+    organization = "r4cc00n"
+
+    workspaces {
+      name = "aws-templates"
+    }
+  }
+}
+
+provider "aws" {
+
+  region = "us-west-1"
+
+  default_tags {
+    tags = {
+      Environment = "Test"
+      Owner       = "TFProviders"
+      Project     = "Test"
+      Name        = "Terraform-Resource"
+    }
+  }
+}
